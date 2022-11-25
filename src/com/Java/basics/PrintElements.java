ef602d3394c4bbe4af2deefc7f4ba5467b4a6df6
VigneshNallasivam <vickyshanestark@gmail.com>
2022-11-25 09:15:50 +0530

    Added : To Print the Elements of aAn Array thst is Assigned Already

---
+++ src/com/Java/basics/PrintElements.java
@@ -0,0 +1,14 @@
+package com.Java.basics;
+
+public class PrintElements
+{
+	public static void main(String[] args) 
+	{    
+        int [] array = {1, 2, 3, 4, 5};  
+        System.out.println("Elements of given array: ");    
+        for (int i = 0; i < array.length; i++) 
+        {  
+            System.out.print(array[i] + " ");  
+        }  
+    }  
+}
